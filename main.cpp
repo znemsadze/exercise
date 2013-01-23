b55ace211e65fd4bbabda79ea0d818cc5e2ca8ba
zcviad <zcviad@zviad-PC>
2013-01-23 09:40:18 +0400

    belman

--- main.cpp
+++ main.cpp
@@ -31,9 +31,11 @@ struct Vert{
 
 
 int currencyExchange(){
+     freopen("input.txt", "rt", stdin);
+   freopen("output.txt", "wt", stdout);
     int n,s,m,kk=0,ind; Edge edges[201];
     float v;Vert verts[101];
-    cin>>n>>s>>m;
+    cin>>n>>m>>s>>v;
     for(int i=0;i<m;i++ ){
         cin>>edges[2*i].s>>edges[2*i].d>>edges[2*i].rate
            >>edges[2*i].comis>>edges[2*i+1].rate>>edges[2*i+1].comis;
@@ -45,12 +47,51 @@ int currencyExchange(){
             if(verts[j].id==edges[2*i].s){ind=ind+2; }
             if(ind==3){break;}
         }
-        if(ind==0){verts[kk]=  edges[2*i].d;kk++; verts[kk]=  edges[2*i].s;kk++; }
-        if(ind==1){verts[kk]=  edges[2*i].d;kk++; }
-        if(ind==2){verts[kk]=  edges[2*i].s;kk++; }
+        if(ind==0){verts[kk].id=  edges[2*i].d;kk++; verts[kk].id=  edges[2*i].s;kk++; }
+        if(ind==1){verts[kk].id=  edges[2*i].d;kk++; }
+        if(ind==2){verts[kk].id=  edges[2*i].s;kk++; }
     }
     
+    for(int i=0;i<kk;i++){
+        if(verts[i].id==s){
+            verts[i].dest=v;
+        }
+        else
+        {verts[i].dest=0;}
+        
+        
+        
+    }
+        
+    for(int i=0; i<kk;i++){
+        for(int j=0;j<m;j++)
+        {
+            if((verts[edges[j].s].dest>0)&&((verts[edges[j].d].dest==0)||
+               (verts[edges[j].d].dest<((verts[edges[j].s].dest-edges[j].comis)*edges[j].rate))
+                    )){
+                verts[edges[j].d].dest=((verts[edges[j].s].dest-edges[j].comis)*edges[j].rate);
+                if(verts[edges[j].d].dest<0){
+                  verts[edges[j].d].dest=0;
+                }
+            }
+        }
+    }
     
+     ind=0;
+        for(int j=0;j<m;j++)
+        {
+            if((verts[edges[j].s].dest>0)&&((verts[edges[j].d].dest==0)||
+               (verts[edges[j].d].dest<((verts[edges[j].s].dest-edges[j].comis)*edges[j].rate))
+                    )){
+                
+                verts[edges[j].d].dest=((verts[edges[j].s].dest-edges[j].comis)*edges[j].rate);
+                if(verts[edges[j].d].dest<0){
+                  verts[edges[j].d].dest=0;
+                }else{ind=1 ;break;}
+            }
+        }
+     string res=(ind==0)?"NO":"YES";
+   cout<<res;
  } 
 
 int main() {
--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=exercise
+PROJECTNAME=CppApplication_1
 
 # Active Configuration
 DEFAULTCONF=Debug
