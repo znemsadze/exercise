e43833493839c70e29321427111e49bd3401cb52
zviad <zviad@zviad-pc>
2014-02-11 17:58:12 +0400

    eeee

--- main.cpp
+++ main.cpp
@@ -18,27 +18,83 @@
 using namespace std;
 
  
+long long R[100000][100000];
+bool ind[100000][100000];
+long m[100000];
 
+long long rcalc(long long i,long long j){
+    if(ind[i][i]){
+    return R[i][j];
+    }
+    R[i][j]=rcalc(i-1,j-1)+m[i];
+ 
+        
+        
+     
+     
+    
+    
+    
+}
+
+
+int  profit(){
+ freopen("input.txt", "rt", stdin);
+  // freopen("output.txt", "wt", stdout);
+  int n;
+  cin>>n;
+  cout<<n;
+  for(int i=0;i<n;i++){
+      
+      cout<<"dfsfdsf\n";
+  
+  
+  }
+  
+    
+}
+
+
+
+int main() {    
+  
+    int k=profit();
+   return 0;
+}
+
+int warmingUp(){
+ freopen("input.txt", "rt", stdin);
+  // freopen("output.txt", "wt", stdout);
+   int n;
+   long x,y,z;
+   long res;long tmp;long tmp1;
+   
+   cin>>n;
+   for(int i=0;i<n;i++){
+       cin>>x>>y>>z;
+       tmp=x/y;res=tmp;
+       while(tmp){
+           tmp1=tmp/z;
+           res+=tmp1;
+           tmp=(tmp1==0)? 0:(tmp- tmp1*z)+tmp1 ;
+ 
+       }
+       cout<<res<<'\n';
+   }
+}
 
 
 
 int squareCountry(){
     long long k;
     cin>>k;
-    int ;
+     
     vector<long long> x(0);
     for(int i=0;i<=sqrt(k);i++){
         x.push_back(i*i);
     }
 
 }
- 
-int main() {
-    int k=squareCountry();
-   return 0;
-}
-
-
 
 
 
